54018d78ac7d238f67b1aff74e98609bc24c890d
vladimir-polyakov <vladimir@forma-pro.com>
2015-07-27 15:59:27 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+browsermob-standalone
+=======================
+
+A jar browsermob proxy standalone wrapper
+
+Current browsermob proxy Standalone version: 2.0 beta 10
---
+++ index.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var path = require("path");
+
+var selenium_server = {
+
+    getServerPath: function () {
+        return path.resolve(__dirname + "/selenium-server-standalone-2.43.1.jar")
+    },
+
+    getChromeDriverPath: function () {
+
+        var chromeDriverPath = '';
+
+        if (process.platform === 'linux' && process.arch === 'x64') {
+            chromeDriverPath = path.resolve(__dirname + "/drivers/linux64");
+        } else if (process.platform === 'linux') {
+            chromeDriverPath = path.resolve(__dirname + "/drivers/linux32/");
+        } else if (process.platform === 'darwin') {
+            chromeDriverPath = path.resolve(__dirname + "/drivers/mac32/");
+        } else if (process.platform === 'win32') {
+            console.log('Windows platform is not supported yet');
+        } else {
+            console.log('Unexpected platform or architecture:', process.platform, process.arch);
+            process.exit(1);
+        }
+
+        return chromeDriverPath
+    }
+};
+
+module.exports = selenium_server; //export
